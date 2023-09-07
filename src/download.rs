cb78500af31e911bb984301fcb322653cc4fb4da
pompompopi <143640209+pompompopi@users.noreply.github.com>
2023-09-07 06:10:26 +0100

    Add progress indicators to downloading & verification tasks

--- src/download.rs
+++ src/download.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
 use anyhow::bail;
+use indicatif::{ProgressBar, ProgressStyle};
 use reqwest::Client;
 use sha1_smol::Sha1;
 use tokio::{
@@ -15,17 +16,29 @@ pub struct Artifact {
     pub path: PathBuf,
 }
 
+fn create_bar(len: u64) -> anyhow::Result<ProgressBar> {
+    let progress_bar = ProgressBar::new(len);
+    let style = ProgressStyle::with_template(
+        "{msg} - [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes}",
+    )?;
+    progress_bar.set_style(style);
+    Ok(progress_bar)
+}
+
 impl Artifact {
     pub async fn download(&self, client: &Client) -> anyhow::Result<()> {
-        // TODO: Add progress indicator
         let mut output = File::create(&self.path).await?;
         let mut res = client.get(&self.url).send().await?;
 
-        info!("Downloading {}...", self.url);
+        let size = res.content_length().unwrap();
+        let progress_bar = create_bar(size)?;
+        progress_bar.set_message(format!("downloading {}", self.url));
         while let Some(chunk) = res.chunk().await? {
             output.write_all(&chunk).await?;
+            progress_bar.set_position((progress_bar.position() + chunk.len() as u64).min(size));
         }
-        info!("Downloaded {}! Checking integrity...", self.url);
+        progress_bar.finish_and_clear();
+        info!("Artifact {} downloaded", self.url);
 
         if !self.properly_exists().await? {
             bail!(
@@ -47,6 +60,10 @@ impl Artifact {
         let mut file = File::open(&self.path).await?;
         let mut buf = vec![0u8; 1048576];
 
+        let len = file.metadata().await?.len();
+        let progress_bar = create_bar(len)?;
+        progress_bar.set_message(format!("hashing {:?}", self.path));
+
         loop {
             let read = file.read(&mut buf).await?;
 
@@ -55,8 +72,16 @@ impl Artifact {
             }
 
             sha1.update(&buf[..read]);
+            progress_bar.set_position((progress_bar.position() + read as u64).min(len));
         }
 
-        Ok(sha1.hexdigest() == self.sha1)
+        let hash = sha1.hexdigest();
+        progress_bar.finish_and_clear();
+        info!(
+            "Completed hashing {:?}, hash is {}",
+            self.path.clone(),
+            hash
+        );
+        Ok(hash == self.sha1)
     }
 }
